2cf4f114afaa1d5ceae6f25df72e946bb18a8d59
Tobias Reusing <tobias@reusing.eu>
2018-03-01 18:17:05 +0100

    Added roster creation

--- faceoff.go
+++ faceoff.go
@@ -1,5 +1,12 @@
 package faceoff
 
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"strings"
+)
+
 type contender int
 
 const (
@@ -9,20 +16,57 @@ const (
 )
 
 type Match struct {
-	contenders [2]string
-	score      [2]int
-	winner     contender
+	Contenders [2]string
+	Score      [2]int
+	Winner     contender
+}
+
+func CreateRoster(participants []byte) (*Roster, error) {
+	buf := bufio.NewScanner(bytes.NewReader(participants))
+	partSlice := make([]string, 0, 16)
+	for buf.Scan() {
+		name := strings.TrimSpace(buf.Text())
+		if len(name) > 1 {
+			partSlice = append(partSlice, name)
+		}
+	}
+	l := len(partSlice)
+	// very ugly, but good enough for now
+	if l != 2 && l != 4 && l != 8 && l != 16 && l != 32 {
+		return nil, errors.New("Unsupported participant number")
+	}
+
+	res := &Roster{Rounds: make([]*Round, 0)}
+	round := &Round{Matches: make([]*Match, 0)}
+
+	for i := 0; i < l; i++ {
+		m := NewMatch(partSlice[i], partSlice[i+1])
+		i++
+		round.Matches = append(round.Matches, m)
+	}
+	res.Rounds = append(res.Rounds, round)
+
+	return res, nil
+
 }
 
 func NewMatch(contenderA string, contenderB string) *Match {
-	m := &Match{contenders: [2]string{contenderA, contenderB}, score: [2]int{0, 0}, winner: none}
+	m := &Match{Contenders: [2]string{contenderA, contenderB}, Score: [2]int{0, 0}, Winner: none}
 	return m
 }
 
 func (m *Match) WinA() {
-	m.score[a]++
+	m.Score[a]++
 }
 
 func (m *Match) WinB() {
-	m.score[b]++
+	m.Score[b]++
+}
+
+type Round struct {
+	Matches []*Match
+}
+
+type Roster struct {
+	Rounds []*Round
 }
--- webserver/webserver.go
+++ webserver/webserver.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 
@@ -15,6 +18,10 @@ import (
 func main() {
 	port := flag.Int("p", 8086, "port number")
 	flag.Parse()
+
+	r := createRoster("values.txt")
+	fmt.Println(&r)
+
 	idxHndlGz := gziphandler.GzipHandler(http.HandlerFunc(indexHandler))
 	http.Handle("/", idxHndlGz)
 	http.Handle("/static/", gziphandler.GzipHandler(http.StripPrefix("/static/", http.FileServer(http.Dir("static")))))
@@ -56,4 +63,21 @@ func templateHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("500 - Something bad happened!" + err.Error()))
 	return
 
+}
+
+func createRoster(filename string) *faceoff.Roster {
+	f, err := os.Open(filename)
+	defer f.Close()
+	if err != nil {
+		panic(err)
+	}
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		panic(err)
+	}
+	r, err := faceoff.CreateRoster(b)
+	if err != nil {
+		panic(err)
+	}
+	return r
 }
